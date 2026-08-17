aaa3e87c9a798a2da12c8348e227be8248614d67
Soobin Park <j.sb.parkk@gmail.com>
2026-08-17 00:24:08 -0700

    update api program.cs

--- CafeGo.Api/Program.cs
+++ CafeGo.Api/Program.cs
@@ -1,6 +1,33 @@
 using CafeGo.Api.Services;
-var builder=WebApplication.CreateBuilder(args);
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Controllers
 builder.Services.AddControllers();
+
+// Google Places service
 builder.Services.AddHttpClient<GooglePlacesService>();
-builder.Services.AddCors(o=>o.AddPolicy("local",p=>p.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod()));
-var app=builder.Build();app.UseCors("local");app.MapControllers();app.Run();
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CafeGoFrontend", policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:5173",
+                "https://cafe-go-swart.vercel.app"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// CORS must run before MapControllers
+app.UseCors("CafeGoFrontend");
+
+app.MapControllers();
+
+app.Run();
